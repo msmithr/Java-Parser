e9f55971a7b4d620e654ea3b8c5a24f4894d57c4
michael <michael.r.smith@siu.edu>
2017-03-16 16:20:11 -0500

    ??

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,6 @@
 
 public class Driver {
-	public static void main(String[] args) {
-
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
