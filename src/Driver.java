a5a6ecc8d609f61a422d3c430322eeb46db19d01
Michael Smith <michael@Michael-Debian.westell.com>
2017-04-05 22:06:34 -0500

    while loops, for loops :^)

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,8 @@
 public class Driver {
     public static void main(String[] args) {
          System.out.println("Goodbye world!");
-	// test test
+    	for (System.out.println("a"), System.out.println("a"); ; ) {
+            //System.out.println("a");
+        }
     }
 }
